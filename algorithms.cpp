7076c727f0ff3f6d6ab40d5838c8ce19109b33cc
rediet <rb.rediet@gmail.com>
2024-10-23 18:10:26 +0300

    format change

--- algorithms.cpp
+++ algorithms.cpp
@@ -2,24 +2,17 @@
 
 using namespace std;
 
-enum class MenuChoice
-{
-    Search,
-    Sort,
-    Exit,
-    Invalid
-};
+enum class MenuChoice { Search,
+                        Sort,
+                        Exit,
+                        Invalid };
 
-enum class SearchChoice
-{
-    Linear,
-    Binary,
-    Back,
-    Invalid
-};
+enum class SearchChoice { Linear,
+                          Binary,
+                          Back,
+                          Invalid };
 
-enum class SortChoice
-{
+enum class SortChoice {
     Simple,
     Bubble,
     Selection,
@@ -50,102 +43,95 @@ int acceptSize();
 void acceptArray(int array[], int size);
 void printArray(int array[], int size, string message = "", bool hold = false);
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int size = acceptSize();
     int array[size];
     acceptArray(array, size);
 
     MenuChoice menuChoice;
-    do
-    {
+    do {
         menuChoice = getMenuChoice();
 
-        switch (menuChoice)
-        {
-        case MenuChoice::Search:
-            SearchChoice searchMenuChoice;
-            do
-            {
-                searchMenuChoice = getSearchChoice();
+        switch (menuChoice) {
+            case MenuChoice::Search:
+                SearchChoice searchMenuChoice;
+                do {
+                    searchMenuChoice = getSearchChoice();
 
-                switch (searchMenuChoice)
-                {
-                case SearchChoice::Linear:
-                    cout << "Linear Search" << endl;
-                    printArray(array, size);
-                    linearSearch(array, size);
-                    break;
+                    switch (searchMenuChoice) {
+                        case SearchChoice::Linear:
+                            cout << "Linear Search" << endl;
+                            printArray(array, size);
+                            linearSearch(array, size);
+                            break;
 
-                case SearchChoice::Binary:
-                    cout << "Binary Search" << endl;
-                    printArray(array, size);
-                    binarySearch(array, size);
-                    break;
+                        case SearchChoice::Binary:
+                            cout << "Binary Search" << endl;
+                            printArray(array, size);
+                            binarySearch(array, size);
+                            break;
 
-                case SearchChoice::Back:
-                    break;
+                        case SearchChoice::Back:
+                            break;
 
-                default:
-                    cout << "Please enter a valid option!" << endl;
-                }
-            } while (searchMenuChoice != SearchChoice::Back);
+                        default:
+                            cout << "Please enter a valid option!" << endl;
+                    }
+                } while (searchMenuChoice != SearchChoice::Back);
 
-            break;
+                break;
 
-        case MenuChoice::Sort:
-            SortChoice sortMenuChoice;
-            do
-            {
-                sortMenuChoice = getSortChoice();
+            case MenuChoice::Sort:
+                SortChoice sortMenuChoice;
+                do {
+                    sortMenuChoice = getSortChoice();
 
-                switch (sortMenuChoice)
-                {
-                case SortChoice::Simple:
-                    cout << "Simple Sort" << endl;
-                    printArray(array, size);
-                    simpleSort(array, size);
-                    break;
+                    switch (sortMenuChoice) {
+                        case SortChoice::Simple:
+                            cout << "Simple Sort" << endl;
+                            printArray(array, size);
+                            simpleSort(array, size);
+                            break;
 
-                case SortChoice::Bubble:
-                    cout << "Bubble Sort" << endl;
-                    printArray(array, size);
-                    bubbleSort(array, size);
-                    break;
+                        case SortChoice::Bubble:
+                            cout << "Bubble Sort" << endl;
+                            printArray(array, size);
+                            bubbleSort(array, size);
+                            break;
 
-                case SortChoice::Selection:
-                    cout << "Selection Sort" << endl;
-                    printArray(array, size);
-                    selectionSort(array, size);
-                    break;
+                        case SortChoice::Selection:
+                            cout << "Selection Sort" << endl;
+                            printArray(array, size);
+                            selectionSort(array, size);
+                            break;
 
-                case SortChoice::Insertion:
-                    cout << "Insertion Sort" << endl;
-                    printArray(array, size);
-                    insertionSort(array, size);
-                    break;
+                        case SortChoice::Insertion:
+                            cout << "Insertion Sort" << endl;
+                            printArray(array, size);
+                            insertionSort(array, size);
+                            break;
 
-                case SortChoice::BinaryInsertion:
-                    cout << "Binary Insertion Sort" << endl;
-                    printArray(array, size);
-                    binaryInsertionSort(array, size);
-                    break;
+                        case SortChoice::BinaryInsertion:
+                            cout << "Binary Insertion Sort" << endl;
+                            printArray(array, size);
+                            binaryInsertionSort(array, size);
+                            break;
 
-                case SortChoice::Back:
-                    break;
+                        case SortChoice::Back:
+                            break;
 
-                default:
-                    cout << "Please enter a valid option!" << endl;
-                }
-            } while (sortMenuChoice != SortChoice::Back);
+                        default:
+                            cout << "Please enter a valid option!" << endl;
+                    }
+                } while (sortMenuChoice != SortChoice::Back);
 
-            break;
+                break;
 
-        case MenuChoice::Exit:
-            break;
+            case MenuChoice::Exit:
+                break;
 
-        default:
-            cout << "Please enter a valid option!" << endl;
+            default:
+                cout << "Please enter a valid option!" << endl;
         }
     } while (menuChoice != MenuChoice::Exit);
 
@@ -154,8 +140,7 @@ int main(int argc, char const *argv[])
 
 // ----------- MENU DEFINITIONS -----------
 
-MenuChoice getMenuChoice()
-{
+MenuChoice getMenuChoice() {
     int choice;
     cout << "\n=========================" << endl;
     cout << "        Main Menu        " << endl;
@@ -167,21 +152,19 @@ MenuChoice getMenuChoice()
     cout << "Enter your choice: ";
     cin >> choice;
 
-    switch (choice)
-    {
-    case 1:
-        return MenuChoice::Search;
-    case 2:
-        return MenuChoice::Sort;
-    case 3:
-        return MenuChoice::Exit;
+    switch (choice) {
+        case 1:
+            return MenuChoice::Search;
+        case 2:
+            return MenuChoice::Sort;
+        case 3:
+            return MenuChoice::Exit;
     }
 
     return MenuChoice::Invalid;
 }
 
-SearchChoice getSearchChoice()
-{
+SearchChoice getSearchChoice() {
     int choice;
     cout << "\n=========================" << endl;
     cout << "      Search Options      " << endl;
@@ -193,21 +176,19 @@ SearchChoice getSearchChoice()
     cout << "Enter your choice: ";
     cin >> choice;
 
-    switch (choice)
-    {
-    case 1:
-        return SearchChoice::Linear;
-    case 2:
-        return SearchChoice::Binary;
-    case 3:
-        return SearchChoice::Back;
+    switch (choice) {
+        case 1:
+            return SearchChoice::Linear;
+        case 2:
+            return SearchChoice::Binary;
+        case 3:
+            return SearchChoice::Back;
     }
 
     return SearchChoice::Invalid;
 }
 
-SortChoice getSortChoice()
-{
+SortChoice getSortChoice() {
     int choice;
     cout << "\n=========================" << endl;
     cout << "       Sort Options       " << endl;
@@ -222,20 +203,19 @@ SortChoice getSortChoice()
     cout << "Enter your choice: ";
     cin >> choice;
 
-    switch (choice)
-    {
-    case 1:
-        return SortChoice::Simple;
-    case 2:
-        return SortChoice::Bubble;
-    case 3:
-        return SortChoice::Selection;
-    case 4:
-        return SortChoice::Insertion;
-    case 5:
-        return SortChoice::BinaryInsertion;
-    case 6:
-        return SortChoice::Back;
+    switch (choice) {
+        case 1:
+            return SortChoice::Simple;
+        case 2:
+            return SortChoice::Bubble;
+        case 3:
+            return SortChoice::Selection;
+        case 4:
+            return SortChoice::Insertion;
+        case 5:
+            return SortChoice::BinaryInsertion;
+        case 6:
+            return SortChoice::Back;
     }
 
     return SortChoice::Invalid;
@@ -243,17 +223,15 @@ SortChoice getSortChoice()
 
 // ----------- ALGORITHM DEFINITIONS -----------
 
-int linearSearch(int array[], int size)
-{
+int linearSearch(int array[], int size) {
     int searchFor;
     cout << "Enter element to search for: ";
     cin >> searchFor;
 
-    for (int i = 0; i < size; i++)
-    {
-        if (array[i] == searchFor)
-        {
-            cout << "Element '" << searchFor << "' found at index '" << i << "'" << endl;
+    for (int i = 0; i < size; i++) {
+        if (array[i] == searchFor) {
+            cout << "Element '" << searchFor << "' found at index '" << i << "'"
+                 << endl;
             return i;
         }
     }
@@ -262,8 +240,7 @@ int linearSearch(int array[], int size)
     return -1;
 }
 
-int binarySearch(int array[], int size)
-{
+int binarySearch(int array[], int size) {
     bubbleSort(array, size);
 
     int searchFor;
@@ -274,19 +251,14 @@ int binarySearch(int array[], int size)
     int middle = size / 2;
     int bottom = size - 1;
 
-    while (top <= bottom)
-    {
-        if (array[middle] == searchFor)
-        {
-            cout << "Element '" << searchFor << "' found at index '" << middle << "'" << endl;
+    while (top <= bottom) {
+        if (array[middle] == searchFor) {
+            cout << "Element '" << searchFor << "' found at index '" << middle
+                 << "'" << endl;
             return middle;
-        }
-        else if (array[middle] < searchFor)
-        {
+        } else if (array[middle] < searchFor) {
             top = middle + 1;
-        }
-        else
-        {
+        } else {
             bottom = middle - 1;
         }
 
@@ -297,16 +269,11 @@ int binarySearch(int array[], int size)
     return -1;
 }
 
-void simpleSort(int array[], int size)
-{
-
+void simpleSort(int array[], int size) {
     int temp;
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = i + 1; j < size; j++)
-        {
-            if (array[i] > array[j])
-            {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (array[i] > array[j]) {
                 temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
@@ -317,15 +284,10 @@ void simpleSort(int array[], int size)
     printArray(array, size, "Sorted Array: ");
 }
 
-void bubbleSort(int array[], int size)
-{
-
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
+void bubbleSort(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
@@ -336,21 +298,16 @@ void bubbleSort(int array[], int size)
     printArray(array, size, "Sorted Array: ");
 }
 
-void selectionSort(int array[], int size)
-{
-
-    for (int i = 0; i < size - 1; i++)
-    {
+void selectionSort(int array[], int size) {
+    for (int i = 0; i < size - 1; i++) {
         int minIndex = i;
 
-        for (int j = i + 1; j < size; j++)
-        {
+        for (int j = i + 1; j < size; j++) {
             if (array[j] < array[minIndex])
                 minIndex = j;
         }
 
-        if (minIndex != i)
-        {
+        if (minIndex != i) {
             int temp = array[i];
             array[i] = array[minIndex];
             array[minIndex] = temp;
@@ -360,15 +317,12 @@ void selectionSort(int array[], int size)
     printArray(array, size, "Sorted Array: ");
 }
 
-void insertionSort(int array[], int size)
-{
-    for (int i = 1; i < size; i++)
-    {
+void insertionSort(int array[], int size) {
+    for (int i = 1; i < size; i++) {
         int current = array[i];
         int tail = i - 1;
 
-        while (tail >= 0 && array[tail] > current)
-        {
+        while (tail >= 0 && array[tail] > current) {
             array[tail + 1] = array[tail];
             tail--;
         }
@@ -378,16 +332,13 @@ void insertionSort(int array[], int size)
     printArray(array, size, "Sorted Array: ");
 }
 
-void binaryInsertionSort(int array[], int size)
-{
-    for (int i = 1; i < size; i++)
-    {
+void binaryInsertionSort(int array[], int size) {
+    for (int i = 1; i < size; i++) {
         int current = array[i];
         int left = 0;
         int right = i;
 
-        while (left < right)
-        {
+        while (left < right) {
             int mid = (left + right) / 2;
             if (array[mid] <= current)
                 left = mid + 1;
@@ -395,8 +346,7 @@ void binaryInsertionSort(int array[], int size)
                 right = mid;
         }
 
-        for (int j = i; j > left; j--)
-        {
+        for (int j = i; j > left; j--) {
             array[j] = array[j - 1];
         }
         array[left] = current;
@@ -405,12 +355,10 @@ void binaryInsertionSort(int array[], int size)
     printArray(array, size, "Sorted Array: ");
 }
 
-int acceptSize()
-{
+int acceptSize() {
     cout << endl;
     int size;
-    do
-    {
+    do {
         cout << "Enter size of the array: ";
         cin >> size;
     } while (size <= 0);
@@ -418,27 +366,23 @@ int acceptSize()
     return size;
 }
 
-void acceptArray(int array[], int size)
-{
+void acceptArray(int array[], int size) {
     int input;
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         cout << "Enter array element " << i + 1 << ": ";
         cin >> input;
         array[i] = input;
     }
 }
 
-void printArray(int array[], int size, string message, bool hold)
-{
+void printArray(int array[], int size, string message, bool hold) {
     if (message.length() > 0)
         cout << message;
     else
         cout << "Array: ";
 
     cout << "[";
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         cout << array[i];
         if (i != size - 1)
             cout << ", ";
